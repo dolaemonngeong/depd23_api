b87662990ecc11d59b4c6b047cef292ee03d0e87
Davina T W <davinawijaya03@gmail.com>
2023-12-10 12:27:57 +0700

    first commit

---
+++ lib/shared/const.dart
@@ -0,0 +1,6 @@
+part of 'shared.dart';
+
+class Const {
+  static String baseURL = 'api.rajaongkir.com';
+  static String apiKey = 'afd70b54176f7c6a293819ea07da29a0';
+}
---
+++ lib/views/pages/pages.dart
@@ -0,0 +1,7 @@
+import 'package:depd23_api/models/models.dart';
+import 'package:depd23_api/services/services.dart';
+import 'package:depd23_api/views/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+
+part 'home.dart';
+part 'fee.dart';
